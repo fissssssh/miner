923d376b17631c6766ded2197d99fc685848d8db
fish <zhangjiancheng2021@outlook.com>
2022-09-04 15:00:57 +0800

    feat: 对限流中间件增加读写锁，防止map并发读取

--- web/middleware/ratelimit.go
+++ web/middleware/ratelimit.go
@@ -2,25 +2,34 @@ package middleware
 
 import (
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/juju/ratelimit"
 )
 
-var buckets map[string]*ratelimit.Bucket = make(map[string]*ratelimit.Bucket)
+var bucketsWrapper = struct {
+	sync.RWMutex
+	Buckets map[string]*ratelimit.Bucket
+}{Buckets: make(map[string]*ratelimit.Bucket)}
 
 func RateLimitMiddleware(fillInterval time.Duration, cap, quantum int64) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ip := c.Request.RemoteAddr
-		if bucket, exists := buckets[ip]; exists {
+		bucketsWrapper.RLock()
+		bucket, exists := bucketsWrapper.Buckets[ip]
+		bucketsWrapper.RUnlock()
+		if exists {
 			if bucket.TakeAvailable(1) < 1 {
 				c.String(http.StatusForbidden, "rate limit...")
 				c.Abort()
 				return
 			}
 		} else {
-			buckets[ip] = ratelimit.NewBucketWithQuantum(fillInterval, cap, quantum)
+			bucketsWrapper.Lock()
+			bucketsWrapper.Buckets[ip] = ratelimit.NewBucketWithQuantum(fillInterval, cap, quantum)
+			bucketsWrapper.Unlock()
 		}
 		c.Next()
 	}
